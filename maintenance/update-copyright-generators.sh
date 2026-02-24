d2324a588eeb12d08c710317dd502163ad68e0dc
Marnix Croes <93143998+MarnixCroes@users.noreply.github.com>
2026-02-24 11:22:53 +0300

    bump copyright to 2026

--- maintenance/update-copyright-generators.sh
+++ maintenance/update-copyright-generators.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-YEAR=$(date +%Y)
+YEAR=2026
 
 find . -path '*/.git' -prune -type f -o -type f -name '*' | xargs sed --in-place -e "s/Copyright (c) 20\([0-9][0-9]\)-20[0-9][0-9] \$(my.name)/Copyright (c) 20\1-$YEAR \$(my.name)/g"
